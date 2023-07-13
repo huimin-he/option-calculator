6d231bae931fe2a63ff54f2539268c9b52319aa0
Huimin He <hhe@brex.com>
2023-07-13 16:57:55 -0400

    turn off darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class', // or 'media' based on preference.
   theme: {
     extend: {
       backgroundImage: {
